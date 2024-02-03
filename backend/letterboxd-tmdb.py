8b666889db65942bba28e2abbc5580b5c8f60b3a
Jessie Yuan <jessieyuan@jessies-mbp-5.wifi.local.cmu.edu>
2024-02-03 13:09:32 -0500

    backend directors processing + embeddings creation + letterboxd integration

--- backend/letterboxd-tmdb.py
+++ backend/letterboxd-tmdb.py
@@ -1,29 +1,51 @@
-import requests, json
+import requests, json, faiss
 from urllib.parse import quote
 from letterboxdpy import user
-from datetime import datetime
+from langchain.embeddings import OpenAIEmbeddings
+from dotenv import load_dotenv
+import numpy as np
+import pandas as pd
 
-user_input = input("Enter your Letterboxd username: ")
-profile = user.User(user_input) 
-print(user.user_diary(profile))
+user_input = "kaitlynng" #input("Enter your Letterboxd username: ")
+favorites = user.User(user_input).favorites
+favorites = [item[0] for item in favorites]
+print(favorites)
 
-url = f"https://api.themoviedb.org/3/search/movie?query={user_input}&include_adult=false&language=en-US&page=1"
+df = pd.read_csv('backend/directors-movies.csv')
+all_recs = []
+index = faiss.read_index("backend/embeddings.index")
+load_dotenv("backend/.env")
+embeddings = OpenAIEmbeddings()
 
-headers = {
-    "accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MzcyNTdjYzlkOTA5M2I3YzNiMWJmMjg2N2U3NGFkNyIsInN1YiI6IjY1YmQ5ZTgwOTMxZWExMDE2Mzk5YjdlMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qzaq5apx4cqANU8JBrBm9JpqqRFGuG_ckEIyAJypvXA"
-}
+for movie in favorites:
+    movie = quote(movie)
+    url = f"https://api.themoviedb.org/3/search/movie?query={movie}&include_adult=false&language=en-US&page=1"
 
-response = requests.get(url, headers=headers)
-print(response.text)
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MzcyNTdjYzlkOTA5M2I3YzNiMWJmMjg2N2U3NGFkNyIsInN1YiI6IjY1YmQ5ZTgwOTMxZWExMDE2Mzk5YjdlMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qzaq5apx4cqANU8JBrBm9JpqqRFGuG_ckEIyAJypvXA"
+    }
 
-data = json.loads(response.text)
-first_id = data['results'][0]['id']
+    response = requests.get(url, headers=headers)
 
-print("First id:", first_id)
+    data = json.loads(response.text)
+    overview = data['results'][0]['overview']
+    
+    test_sample = embeddings.embed_query(overview)
+    test_sample = np.array([test_sample]).astype("float32")
+    k = 5
+    distances, indices = index.search(test_sample, k)
 
-url = f"https://api.themoviedb.org/3/movie/{first_id}/recommendations?language=en-US&page=1"
+    recs = df.iloc[indices[0]]
+    recs_dict = recs.to_dict(orient='records')
+    all_recs += recs_dict
 
-response = requests.get(url, headers=headers)
+seen = set()
+unique_list_of_recs = []
 
-print(response.text)
+for d in all_recs:
+    if d['title'] not in seen:
+        unique_list_of_recs.append(d)
+        seen.add(d['title'])
+
+print(unique_list_of_recs)
